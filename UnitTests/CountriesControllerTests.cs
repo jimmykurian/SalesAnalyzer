9a329c05c16e754cf13b774b57c09d7b89ca503e
Jimmy Kurian <kurianjimmy11@gmail.com>
2020-11-27 18:19:44 -0600

    feat(unittests): add country controller unit tests

--- UnitTests/CountriesControllerTests.cs
+++ UnitTests/CountriesControllerTests.cs
@@ -1,5 +1,5 @@
-using API.Controllers;
-using Application.States.Queries;
+﻿using API.Controllers;
+using Application.Countries.Queries;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -12,45 +12,45 @@ using Xunit;
 
 namespace UnitTests
 {
-    public class StatesControllerTests
+    public class CountriesControllerTests
     {
         private Mock<IMediator> _mediator;
 
-        public StatesControllerTests()
+        public CountriesControllerTests()
         {
             _mediator = new Mock<IMediator>();
         }
 
         [Fact]
-        public void Get_List_Of_States()
+        public void Get_List_Of_Countries()
         {
             // Arrange
             _mediator
                 .Setup(x => x.Send(It.IsAny<List.Query>(), new CancellationToken()))
-                .ReturnsAsync(new List<State>());
-            var statesController = new StatesController(_mediator.Object);
+                .ReturnsAsync(new List<Country>());
+            var countriesController = new CountriesController(_mediator.Object);
 
             // Act
-            var result = statesController.List();
+            var result = countriesController.List();
 
             // Assert 
-            Assert.IsType<Task<ActionResult<List<State>>>>(result);
+            Assert.IsType<Task<ActionResult<List<Country>>>>(result);
         }
 
         [Fact]
-        public void Get_State_By_Id()
+        public void Get_Country_By_Id()
         {
             // Arrange
             _mediator
                 .Setup(x => x.Send(It.IsAny<Details.Query>(), new CancellationToken()))
-                .ReturnsAsync(new State());
-            var statesController = new StatesController(_mediator.Object);
+                .ReturnsAsync(new Country());
+            var countriesController = new CountriesController(_mediator.Object);
 
             // Act
-            var result = statesController.Details(Guid.NewGuid());
+            var result = countriesController.Details(Guid.NewGuid());
 
             // Assert 
-            Assert.IsType<Task<ActionResult<State>>>(result);
+            Assert.IsType<Task<ActionResult<Country>>>(result);
         }
     }
 }
